b9fb6d46c93a366ca7f7d6cf0ed96b9e7c200b9b
Dragan Savic <dragan@4max.com>
2021-11-30 23:08:37 +0100

    add new pins with border; change dots color for viewpager; add scroll for about screen

--- android/src/com/mileticgo/app/view/AboutFragment.kt
+++ android/src/com/mileticgo/app/view/AboutFragment.kt
@@ -1,13 +1,13 @@
 package com.mileticgo.app.view
 
 import android.os.Bundle
+import android.text.method.ScrollingMovementMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
 import com.mileticgo.app.R
 import com.mileticgo.app.databinding.AboutFragmentBinding
 
@@ -20,9 +20,7 @@ class AboutFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.about_fragment, container, false)
 
-        /*binding.myToolbar.setNavigationOnClickListener {
-            findNavController().popBackStack()
-        }*/
+        binding.tvAboutText.movementMethod = ScrollingMovementMethod()
 
         return binding.root
     }
